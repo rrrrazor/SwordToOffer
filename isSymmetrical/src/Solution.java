e6da0ac28c579707affc30fee4ca0f0a60ed36d0
rrrrazor <rrwutongyu@163.com>
2019-04-09 22:24:11 +0800

    first commit

---
+++ isSymmetrical/src/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    static boolean isSymmetrical(TreeNode pRoot)
+    {
+        return isSymmetrical(pRoot, pRoot);
+
+    }
+    public static boolean isSymmetrical(TreeNode pRoot1, TreeNode pRoot2){
+        if(pRoot1 == null && pRoot2 == null){
+            return true;
+        }
+        if(pRoot1 == null || pRoot2 == null){
+            return false;
+        }
+        if(pRoot1.val != pRoot2.val){
+            return false;
+        }
+        return isSymmetrical(pRoot1.left, pRoot2.right) && isSymmetrical(pRoot1.right, pRoot2.left);
+    }
+}
